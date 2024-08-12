735fa8724e09006fc79c31701e2fae2684d2e2a0
Daniel Skrobot <dd7412506@gmail.com>
2024-08-12 10:06:12 +0200

    fix

--- init.lua
+++ init.lua
@@ -162,7 +162,7 @@ vim.opt.scrolloff = 10
 --  See `:help hlsearch`
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
+vim.keymap.set('n', '<leader>gb', vim.cmd.bprevious)
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
